fa04bb1df596f50206769874f705038152972dd5
rawan-abdelrahim <rawanabdo7575@gmail.com>
2025-08-02 12:36:24 +0300

    Create Category

---
+++ AseerAlkotb.Application/Features/Categories/Validators/DeleteCategoryRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using AseerAlkotb.Application.Features.Categories.Requests;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AseerAlkotb.Application.Features.Categories.Validators
+{
+    public class DeleteCategoryRequestValidator : AbstractValidator<DeleteCategoryRequest>
+    {
+        public DeleteCategoryRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("Category ID must be greater than 0");
+        }
+    }
+}
---
+++ AseerAlkotb.Application/Services/CategoryServices.cs
@@ -0,0 +1,28 @@
+﻿using AseerAlkotb.Application.Contracts;
+using AseerAlkotb.Application.Features.Authors.Requests;
+using AseerAlkotb.Application.Features.Authors.Responses;
+using AseerAlkotb.Application.Features.Authors.Validators;
+using AseerAlkotb.Application.ResponseHandler;
+using AseerAlkotb.Domain.Entites.Models;
+using AseerAlkotb.Domain.Interfaces.Base;
+using Mapster;
+using Microsoft.Extensions.Hosting;
+using static AseerAlkotb.Application.ResponseHandler.ApiResponseHandler;
+
+namespace AseerAlkotb.Application.Services
+{
+    public class CategoryServices : AppService, ICategoryServices
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CategoryServices(IUnitOfWork unitOfWork, IServiceProvider serviceProvider, IHostEnvironment environment) : base(serviceProvider, environment)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ApiResponse<AddCategoryResponse>>AddCategoryAsync(AddCategoryRequest request)
+        {
+            await DoValidationAsync<AddCategoryRequestValidator, AddCategoryRequest>(request);
+        }
+    }
+}
