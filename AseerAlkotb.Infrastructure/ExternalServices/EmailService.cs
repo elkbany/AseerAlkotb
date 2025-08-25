05eb74b93c644bcbdada0918aa82d2a140a05fee
elkbany <mahmuodelkbany50@gmail.com>
2025-08-25 17:53:51 +0300

    Enhance account management and email functionality [You Should Type Your App Password in AppSettings.json to able to use Email Service] Updated AccountController to support email confirmation, password resets, and improved error handling. Refactored AccountService and IAccountServices interface to implement new methods. Added IEmailService for email notifications and SMTP configuration in appsettings.json. Enhanced data seeding with new categories and authors. Introduced NotificationController and related request classes for email handling. Updated migrations to reflect new data structure.

---
+++ AseerAlkotb.Infrastructure/ExternalServices/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using AseerAlkotb.Application.Contracts.External;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AseerAlkotb.Infrastructure.ExternalServices
+{
+    public class EmailService : IEmailService
+    {
+        private readonly string _smtpServer;
+        private readonly int _port;
+        private readonly string _email;
+        private readonly string _appPassword;
+        private readonly bool _isBodyHtml;
+        public EmailService(IConfiguration configuration)
+        {
+            var emailSettings = configuration.GetSection("EmailSettings");
+            _smtpServer = emailSettings["SmtpServer"];
+            _port = int.Parse(emailSettings["Port"]);
+            _email = emailSettings["Email"];
+            _appPassword = emailSettings["AppPassword"];
+            _isBodyHtml = bool.Parse(emailSettings["IsBodyHtml"]);
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using (var client = new SmtpClient(_smtpServer, _port))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(_email, _appPassword);
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_email),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = _isBodyHtml
+                };
+
+                mailMessage.To.Add(to);
+
+                await client.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
