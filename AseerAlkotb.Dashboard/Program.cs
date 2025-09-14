77d3abee6974c057c67e10a0673e75e57126465a
Nadasae <nadasaeed566@gmail.com>
2025-09-14 14:04:05 +0300

    edits

--- AseerAlkotb.Dashboard/Program.cs
+++ AseerAlkotb.Dashboard/Program.cs
@@ -23,6 +23,19 @@ using AseerAlkotb.Application.BackgroundJobs;
 using Polly.Extensions.Http;
 using Polly;
 using System.Net;
+using AseerAlkotb.Application.Features.Account.Validator;
+using AseerAlkotb.Application.Features.Authors.Validators;
+using AseerAlkotb.Application.Features.Books.Validators;
+using AseerAlkotb.Application.Features.CartItems.Validation;
+using AseerAlkotb.Application.Features.Categories.Validators;
+using AseerAlkotb.Application.Features.Orders.Validators;
+using AseerAlkotb.Application.Features.Payments.Validators;
+using AseerAlkotb.Application.Features.Publishers.Validators;
+using AseerAlkotb.Application.Features.Quotes.Validators;
+using AseerAlkotb.Application.Features.Rag.Validators;
+using AseerAlkotb.Application.Features.Reviews.Validators;
+using AseerAlkotb.Application.Features.Roles.Validators;
+using AseerAlkotb.Application.Features.Wishlist.Validators;
 
 namespace AseerAlkotb.Dashboard
 {
@@ -103,7 +116,111 @@ namespace AseerAlkotb.Dashboard
             // New services for improved Order and Payment flow
             builder.Services.AddScoped<IOrderPaymentSyncService, OrderPaymentSyncService>();
             builder.Services.AddScoped<IPaymentRetryService, PaymentRetryService>();
+            #region Validators 
+            // Account Validators
+            builder.Services.AddScoped<GetProfileRequestValidator>();
+            builder.Services.AddScoped<LoginRequestValidator>();
+            builder.Services.AddScoped<RegisterRequestValidator>();
+            builder.Services.AddScoped<ResetPasswordRequestValidator>();
+            builder.Services.AddScoped<UpdateProfileRequestValidator>();
 
+            // Authors Validators
+            builder.Services.AddScoped<AddAuthorRequestValidator>();
+            builder.Services.AddScoped<DeleteAuthorRequestValidator>();
+            builder.Services.AddScoped<FollowAuthorRequestValidation>();
+            builder.Services.AddScoped<GetAllAuthorsPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetAutherFollowerCountRequestValidation>();
+            builder.Services.AddScoped<GetFollowedAuthorRequestValidatin>();
+            builder.Services.AddScoped<GetFollowersAuthorRequestValidation>();
+            builder.Services.AddScoped<UnFollowAuthorRequestValidation>();
+            builder.Services.AddScoped<UpdateAuthorRequestValidator>();
+            builder.Services.AddScoped<GetAuthorByIdRequestValidator>();
+
+
+            // Books Validators
+            builder.Services.AddScoped<AddBookRequestValidator>();
+            builder.Services.AddScoped<DeleteBookRequestValidator>();
+            builder.Services.AddScoped<FilterBooksRequestValidator>();
+            builder.Services.AddScoped<GetAllBooksPaginatedValidator>();
+            builder.Services.AddScoped<GetBookByIdRequestValidator>();
+            builder.Services.AddScoped<UpdateBookRequestValidator>();
+
+            // Cart Items Validators
+            builder.Services.AddScoped<AddCartItemValidation>();
+            builder.Services.AddScoped<ClearCartRequestValidation>();
+            builder.Services.AddScoped<DeleteItemValidation>();
+            builder.Services.AddScoped<ShowCartRequestValidation>();
+            builder.Services.AddScoped<UpdateItemQuantityValidation>();
+
+            // Category Validators
+            builder.Services.AddScoped<AddCategoryRequestValidator>();
+            builder.Services.AddScoped<AddSubCategoryRequestValidator>();
+            builder.Services.AddScoped<DeleteCategoryRequestValidator>();
+            builder.Services.AddScoped<DeleteSubCategoryRequestValidator>();
+            builder.Services.AddScoped<GetAllCategoriesPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetAllSubCategoriesPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetCategoryByIdRequestValidator>();
+            builder.Services.AddScoped<UpdateCategoryRequestValidator>();
+
+            // Order Validators
+            builder.Services.AddScoped<AddOrderRequestValidator>();
+            builder.Services.AddScoped<CancelOrderRequestValidator>();
+            builder.Services.AddScoped<GetOrderByAdminByTrackingNumberRequestValidator>();
+            builder.Services.AddScoped<GetAllUserOrdersPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetOrderByAdminByTrackingNumberRequestValidator>();
+            builder.Services.AddScoped<GetUserOrderByTrackingNumberRequestValidator>();
+            builder.Services.AddScoped<OrderItemDTOValidator>();
+            builder.Services.AddScoped<UpdateOrderStatusRequestValidator>();
+            builder.Services.AddScoped<GetAllOrdersPaginatedRequestValidator>();
+
+            // Payment Validators
+            builder.Services.AddScoped<InitializePaymentRequestValidator>();
+            builder.Services.AddScoped<UpdatePaymentStatusRequestValidator>();
+
+            // Publisher Validators
+            builder.Services.AddScoped<AddPublisherRequestValidator>();
+            builder.Services.AddScoped<DeletePublisherRequestValidator>();
+            builder.Services.AddScoped<FollowPublisherRequestValidation>();
+            builder.Services.AddScoped<GetAllPublishersPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetFollowedPublisherRequestValidation>();
+            builder.Services.AddScoped<GetFollowersPublisherRequestValidation>();
+            builder.Services.AddScoped<GetPublisherByIdRequestValidator>();
+            builder.Services.AddScoped<GetPublisherFollowerCountRequestValidation>();
+            builder.Services.AddScoped<UnFollowPublisherRequestValidation>();
+            builder.Services.AddScoped<UpdatePublisherRequestValidator>();
+
+            // Quote Validators
+            builder.Services.AddScoped<AddQuoteRequestValidator>();
+            builder.Services.AddScoped<DeleteQuoteRequestValidator>();
+            builder.Services.AddScoped<GetAllQuotePaginatedRequestValidator>();
+            builder.Services.AddScoped<GetByIdQuoteRequestValidator>();
+            builder.Services.AddScoped<UpdateQuoteRequestValidator>();
+
+            // Rag Validators
+            builder.Services.AddScoped<RagAskRequestValidator>();
+
+            // Review Validators
+            builder.Services.AddScoped<AddReviewRequestValidator>();
+            builder.Services.AddScoped<DeleteReviewRequestValidator>();
+            builder.Services.AddScoped<GetAllReviewsPaginatedRequestValidator>();
+            builder.Services.AddScoped<GetReviewByIdRequestValidator>();
+            builder.Services.AddScoped<UpdateReviewRequestValidator>();
+
+            // Role Validators
+            builder.Services.AddScoped<AssignRoleRequestValidator>();
+            builder.Services.AddScoped<CreateAdminAccountRequestValidator>();
+            builder.Services.AddScoped<DeleteAdminAccountRequestValidator>();
+            builder.Services.AddScoped<RemoveRoleRequestValidator>();
+            builder.Services.AddScoped<UpdateAdminAccountRequestValidator>();
+
+            // Wishlist Validators
+            builder.Services.AddScoped<AddWishlistItemValidation>();
+            builder.Services.AddScoped<ClearWishlistValidation>();
+            builder.Services.AddScoped<DeleteWishlistItemValidation>();
+            builder.Services.AddScoped<GetUserWishlistValidation>();
+            builder.Services.AddScoped<GetWishlistItemCountValidation>();
+            builder.Services.AddScoped<IsBookInWishlistValidation>();
+            #endregion
             // Add other services needed by the dashboard controllers
 
             // RAG deps (Embedding + Router)
